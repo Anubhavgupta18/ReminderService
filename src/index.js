59ee7c8b693fffdfa5b2c0f581d2e87bd2e4ba7e
anubhav <anubhavshivahre@gmail.com>
2023-02-05 12:03:03 +0530

    added emailing service

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
+const { sendMail } = require('./services/emailing-service');
 
 const createAndStartServer = () => {
     const app = express();
@@ -10,6 +11,14 @@ const createAndStartServer = () => {
     app.listen(PORT, () => {
         console.log(`Server started on PORT : ${PORT}`);
     })
+
+    // sendMail(
+    //     'anubhavgupta8769@gmail.com',
+    //     'anubhavshivahre@gmail.com',
+    //     'This is testing email',
+    //     'Hello there! this is a email to check the service'
+    // );
+
 }
 
 createAndStartServer();
